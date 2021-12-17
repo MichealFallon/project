4ee3c36a486c4ae9360630578c962ecc9405a41a
MichealFallon <78159517+MichealFallon@users.noreply.github.com>
2021-12-17 13:42:17 +0000

    first commit

---
+++ Project/custom-js/custom.js
@@ -0,0 +1,52 @@
+
+if (localStorage.getItem('checkout') == null) {  
+    localStorage.setItem('checkout',0);
+}
+var checkout=localStorage.getItem('checkout');
+document.querySelector('#checkout').innerHTML=checkout;
+
+var logout = document.getElementById('loginlogout');
+logout.addEventListener("click", Logout);
+
+function Logout()
+{
+    var loggedin=localStorage.getItem('loggedIn'); 
+    if (loggedin==1) {
+        localStorage.setItem('loggedIn',0);
+        window.location.href = "home.html";
+    } else {
+        window.location.href = "login.html";
+    }
+}
+
+
+// check if user is logged in or logged out..
+checkLoginStatus()
+
+function checkLoginStatus() {
+    
+    var loggedin=localStorage.getItem('loggedIn'); 
+    var element = document.getElementById("userdetails");
+    if (loggedin==1) {
+        // change the text from Login to Logout
+        document.querySelector('#loginlogout').innerHTML="Logout";
+        element.classList.remove("d-none");        
+        element.classList.add("d-show");      
+    } else{
+        // use add to hide the display of User Details
+        element.classList.add("d-none");        
+        element.classList.remove("d-show");
+        document.querySelector('#loginlogout').innerHTML="Login"; 
+        element = document.getElementById("loginlogout");
+        element.setAttribute("href", "login.html");
+    } 
+
+}
+document.getElementById("buy-button").addEventListener("click",buy,false)
+function buy(){
+    localStorage.setItem("checkout",0)
+}
+
+
+
+
---
+++ Project/custom-js/login.js
@@ -0,0 +1,26 @@
+var loginStatus = document.getElementById('user-login');
+// add a listener for add to cart if such a button id is pressed
+loginStatus.addEventListener("submit", loginUser);
+
+function loginUser() {
+
+    // wait for submit button to be clicked on login form - 
+    // this code only invoked if login form submit button clicked
+
+    let email = document.getElementById('emailAddressID').value;
+    let password= document.getElementById('passwordID').value;
+    if (email=="email@email.com" && password=="password")  {   
+        // successful login, user redirected to shop.html
+        localStorage.setItem('loggedIn',1);    
+        window.location.href = "shop.html";  // redirect to shop page
+    }
+    else {
+        // login unsuccessful, error message appears
+        localStorage.setItem('loggedIn',0);
+        var element = document.getElementById("loginerror");
+        element.classList.remove("d-none");
+       // element.classList.remove("d-block");
+    }
+    event.preventDefault();
+}
+
